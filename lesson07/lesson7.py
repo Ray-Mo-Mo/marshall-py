c69e02030cdb56ae8e15976028bfe672844772f9
Ray-Mo-Mo <raymo1269@gmail.com>
2025-02-21 13:56:30 -0500

    solved lesson 7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,14 @@
 # Lesson 7
+
+from random import randrange
+difficulty = int(input("Enter the DC: "))
+
+player_roll = randrange(1, 21)
+
+print(f"the playes has rolled {player_roll} on their D20.")
+
+
+if player_roll >= difficulty:
+    print(f"the player was successful at {player_roll} >= {difficulty}")
+else:
+        print(f"the player was not successful at {player_roll} < {difficulty}")
