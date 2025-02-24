ff74c1f6d93376aee891587e43d53ab2887cd0fa
Ray-Mo-Mo <raymo1269@gmail.com>
2025-02-24 13:52:48 -0500

    solved lesson 9

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,21 @@
 # Lesson 9
+
+import random
+
+while True:
+    choices = ['rock', 'paper', 'scissors']
+    bot = choices[random.randrange(len(choices))]
+
+    if player == 'done':
+        break
+        
+    player = input(f"Rock/Scissor/Paper: ")
+
+    if player == bot:
+        print(f"tie.")
+
+    if player == 'rock' and bot == 'scissors' or player == 'scissors' and bot == 'paper' or player == 'paper' and bot == 'rock':
+        print(f"the bot picked {bot}, you win.")
+    else:
+        print(f"the bot picked {bot}, you lost.")
+
