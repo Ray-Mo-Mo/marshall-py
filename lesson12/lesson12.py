8baa6266a05375cd57bbe72accb225b3cf177390
Ray-Mo-Mo <raymo1269@gmail.com>
2025-02-26 09:14:58 -0500

    solved lesson 12

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,23 @@
 # Lesson 12
+
+angles = input("Enter 3 angles: ")
+
+angle_list = list(map(int, angles.split()))
+
+total_angle = sum(angle_list)
+
+if len(angle_list) != 3:
+    print("please enter values with 3")
+else: 
+    a1, a2, a3 = map(int, angle_list)
+    print(a1, a2, a3, total_angle)
+
+if total_angle == 180:
+    if a1 == a2 == a3:
+        print("Equilateral")
+    elif a1 == a2 or a1 == a3 or a2 == a3:
+        print("Isosleces")
+    else:
+        print("Scalene")
+else:
+    print("not a triangle")
