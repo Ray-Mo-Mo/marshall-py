20f90397b7e8c8e95abd5a70c16126e2ecc704b8
Ray-Mo-Mo <raymo1269@gmail.com>
2025-02-26 07:53:24 -0500

    solved lesson 11

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,13 @@
 # Lesson 11
+
+x_input = int(input(f"Enter the x value: "))
+y_input = int(input(f"Enter the y value: "))
+
+if x_input >= 0 and y_input >= 0:
+    print(f"quadrant 1")
+elif x_input <= 0 and y_input >= 0:
+    print(f"quadrant 2")
+elif x_input >= 0 and y_input <= 0:
+    print(f"quadrant 4")
+elif x_input <= 0 and y_input <= 0:
+    print(f"quadrant 3")
