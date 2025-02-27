a76767e48bf3f9242168d2ec4de1b732d70fff18
Ray-Mo-Mo <raymo1269@gmail.com>
2025-02-26 22:06:48 -0500

    solved lesson19

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,12 @@
 # Lesson 19
+num = int(input(f"Enter a number: "))
+counter = 0
+
+for divider in range(1, num+1):
+    if num % divider == 0:
+        counter += 1
+
+if counter == 2:
+    print(f"prime")
+else:
+    print(f"composite")
