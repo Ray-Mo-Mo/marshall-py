387527b9fbd15ddebed844a7803adbc13180aad4
Ray-Mo-Mo <raymo1269@gmail.com>
2025-02-26 19:58:37 -0500

    solved lesson 16

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,15 @@
 # Lesson 16
+
+num = int(input("Enter a number between 1 - 50: "))
+
+r1 = num % 5
+r2 = num % 3
+
+if r1 == 0 and r2 == 0:
+    print("Number is a multiple of 5 and 3")
+elif r1 == 0:
+    print("Number is a multiple of 5")
+elif r2 == 0:
+    print("Number is a multiple of 3")
+else:
+    print("Number is not a multiple of 5 or 3")
