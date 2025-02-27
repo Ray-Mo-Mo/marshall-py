bf494a93b72e6bf1e16c69faf8d061646b820d2b
Ray-Mo-Mo <raymo1269@gmail.com>
2025-02-26 23:28:21 -0500

    solved lesson 23

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,17 @@
 # Lesson 23
+
+x = input("enter number, done to stop: ")
+counter = 0
+total = 0
+
+while True:
+    if x == "done":
+        break
+    else:
+        total += int(x)
+        counter += 1
+        x = input()
+
+average = total / counter
+print(average)
+        
