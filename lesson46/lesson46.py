a05b507eade71c79ad2e47ff2580445559c7242b
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-26 09:17:40 -0400

    solved lesson 46

--- lesson46/lesson46.py
+++ lesson46/lesson46.py
@@ -1,1 +1,13 @@
 # Lesson 46
+
+num = int(input("enter a number: "))
+
+counter = 0
+
+while num != 1:
+    if num % 2 == 1:
+        num = 3*num+1
+        print(num)
+    else:
+        num = num / 2
+        print(num)
