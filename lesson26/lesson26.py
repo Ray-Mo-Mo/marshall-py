4dc55d7a5d43300dbceaedab080ee05f0663ee0c
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-26 08:26:28 -0400

    solved lesson 26

--- lesson26/lesson26.py
+++ lesson26/lesson26.py
@@ -1,1 +1,19 @@
 # Lesson 26
+
+def divisible(num1, num2): # define the variable for if a number is divisible
+    return num1 % num2 == 0
+
+def factor(number): # function for if a number is factor
+    counter = 0
+    for i in range(1, number + 1):
+        if divisible(number, i):
+            counter += 1
+    return counter
+
+upper_limit = int(input("Enter a number: "))
+
+for num in range(1, upper_limit+1):
+    factor_size = factor(num)
+        
+    print(f"The number {num} has {factor_size} factors")
+
