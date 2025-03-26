1923895559d7f3023f4fdffcb331422fd59467ce
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-26 08:42:16 -0400

    solved  lesson 27

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,11 @@
 # Lesson 27
+
+def string_cleaner(text):
+    result = ''
+    for character in text:
+        if character.isalpha():
+            result += character.lower()
+    return result
+
+value = string_cleaner('HeLLo, wORLd!')
+print(f"The value is: {value}")
