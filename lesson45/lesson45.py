296864da353c007610901943e68fd2980bc4a330
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-26 13:58:25 -0400

    solved lesson 45

--- lesson45/lesson45.py
+++ lesson45/lesson45.py
@@ -1,1 +1,14 @@
 # Lesson 45
+
+fruits = ["apple", "banana", "cherry", "date"]
+print(fruits)
+user_input = str(input("What fruit: "))
+
+counter = 0
+
+if user_input in fruits:
+    for i in range(1, len(user_input) + 1):
+        counter += 1
+    print(f"fruit found, it has {counter} amount of characters")
+else:
+    print("fruit not found")
