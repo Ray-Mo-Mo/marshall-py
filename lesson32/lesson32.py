bf0e1fe3a38f4a09aaeaed4dd896eef3642c8eaa
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-28 09:12:22 -0400

    solved lesson 32

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,24 @@
 # Lesson 32
+
+# word = []
+
+# def sort(text):
+#     for word in range(1, len(text)):
+#         if word in text:
+#             word.append(text)
+#     return word
+
+# text = 'eerrddffss'
+# print(sort(text))
+
+def duplicated(word1, word2):
+    if  not word1 or not word2:
+        return []
+    else:
+        set_word2 = set(word2)
+        result = []
+        for character in word1:
+            result.append(character)
+        return sorted(result)
+
+print(duplicated("hello world", "goodbye world"))
