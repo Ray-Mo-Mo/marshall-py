77f13a4e7fa954eed80ce94d7d4a0d34ec8e043c
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-28 07:57:52 -0400

    solved lesson 43

--- lesson43/lesson43.py
+++ lesson43/lesson43.py
@@ -1,1 +1,10 @@
 # Lesson 43
+def duplicates(a_list):
+    result = []
+
+    for value in a_list:
+        if value not in result:
+            result.append(value)
+    return result
+
+print(duplicates([1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 4, 4]))
