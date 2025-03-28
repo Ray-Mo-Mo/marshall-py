4cb3ea6973afe446a180b05bced9cd547c2188de
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-28 08:06:30 -0400

    solved lesson 42

--- lesson42/lesson42.py
+++ lesson42/lesson42.py
@@ -1,1 +1,14 @@
 # Lesson 42
+
+def possible_sums(a_list, target):
+    for i, value in enumerate(a_list):
+        new_target = target - value
+        if new_target in a_list[i+1:]:
+            return True
+    return false
+
+test = [1, 3, 5, 8, 12, 13, 22]
+target = 16
+
+print(f"can {target} be made from {test}?")
+print(possible_sums(test, target))
