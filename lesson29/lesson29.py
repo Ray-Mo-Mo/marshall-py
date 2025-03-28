f40e68128f8cfafce5cc4ef6ea7e689a0a6d13bf
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-28 08:16:37 -0400

    solved lesson 29

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,12 @@
 # Lesson 29
+
+counter = 0
+
+def consonant(text):
+    word = input("what word: ")
+    if word in text:
+        for i in range(word):
+            if word.isalpha() and word in {'a', 'e', 'i', 'o', 'u'}
+                counter += 1
+
+print(f"your word have {counter} consanats")
