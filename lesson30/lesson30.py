5ac806b2e21dd602377df060c5a7d4ce645397ed
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-28 08:54:03 -0400

    solved lesson 30

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,16 @@
 # Lesson 30
+
+def n_create(n):
+    text = ''
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            n += '1'
+        else:
+            n += '0'
+
+def n_output(n):
+    for i in range(1, n+1):
+        print(n_create(i))
+
+v = n_output(5)
+print(v)
