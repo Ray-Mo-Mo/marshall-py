cc6c506d8edcf809b89a92c7c3e80cfec14beef1
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-28 08:09:00 -0400

    solved lesson 28

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,16 @@
 # Lesson 28
+def anagram_checker(t, s):
+
+    arrT = list(t) 
+    arrS = list(s)
+
+    arrT.sort()
+    arrS.sort()
+
+    if(arrT == arrS):
+        return True
+
+    return False
+
+
+print(anagram_checker("racecar", "carrace"))
