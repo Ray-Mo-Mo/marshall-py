1bc091cdddbbcddbd94becc29260d4086e3532f7
Ray-Mo-Mo <raymo1269@gmail.com>
2025-03-28 08:56:01 -0400

    solved lesson 31

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,17 @@
 # Lesson 31
+
+def anagram_checker(t, s):
+
+    arrT = list(t) 
+    arrS = list(s)
+
+    arrT.sort()
+    arrS.sort()
+
+    if(arrT == arrS):
+        return True
+
+    return False
+
+
+print(anagram_checker("racecar", "carrace"))
