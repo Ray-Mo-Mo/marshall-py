c641e56f71a523ff6af4a253573cc6edfd686bb7
Ray-Mo-Mo <raymo1269@gmail.com>
2025-04-09 17:28:00 -0400

    solved lesson 34

--- lesson34/lesson34.py
+++ lesson34/lesson34.py
@@ -1,1 +1,24 @@
 # Lesson 34
+
+def csv2list(text):
+    csv = text.split()
+    print(csv)
+    a_list = []
+
+    for item in csv:
+        a_list.append(int(item))
+
+    return a_list
+
+from random import randrange
+def rand_list(start, end, frequency):
+    if start < end and frequency > 0: 
+        new_value = randrange(start, end+1)
+        a_list.append(new_value)
+    return a_list
+    else:
+        return []
+
+
+print(csv2list("1, 2, 3, 4, 5, 6"))
+print(randlist(1, 1000, 30))
