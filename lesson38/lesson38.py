8218fea33dc4828f17e4ccdef1f7aaa8d7e4d400
Ray-Mo-Mo <raymo1269@gmail.com>
2025-04-09 20:49:58 -0400

    solved lesson 41

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,12 @@
 # Lesson 38
+
+def palindrone(text):
+    return text == text[::-1]
+
+p_num = []
+
+for num1 in range(999, 99, -1):
+    for num2 in range(999, 99, -1):
+        current = num1, num2
+        if palindrone(str(current)):
+            p_num.append(current)
--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,7 @@
 # Lesson 39
+
+def gcd(num1, num2):
+    for divider in range(min(num1, num2), 0 , -1):
+        if num1 % divider == 0 and num2 % divider == 0:
+            return divider
+    return 1
